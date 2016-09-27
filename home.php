a96c98319b6bd627b6740627136f98a7221d9291
bubbles <bubbleblastcovers@gmail.com>
2016-09-27 13:57:48 +0800

    Added button

--- home.php
+++ home.php
@@ -3,5 +3,7 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
+	<div class='col-md-8 col-md-offset 2'>
+		<a href='index.php'><button class='btn btn-danger'>I HAVE AN APPLE, I HAVE A PEN. UGH! APPLE PEN!</button></a>
 </body>
 </html>

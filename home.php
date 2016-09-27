6f06ba215c5fdd35d87e7a81ce30d41d91b4c170
penuel456 <penuelcalle@gmail.com>
2016-09-27 14:15:34 +0800

    Added glyphicon

--- home.php
+++ home.php
@@ -4,6 +4,11 @@
 </head>
 <body>
 	<div class='col-md-8 col-md-offset 2'>
-		<a href='index.php'><button class='btn btn-danger'>I HAVE AN APPLE, I HAVE A PEN. UGH! APPLE PEN!</button></a>
+		<a href='index.php'>
+			<button class='btn btn-danger'>
+				<span class='glyphicon glyphicon-user'></span>
+				I HAVE AN APPLE, I HAVE A PEN. UGH! APPLE PEN!
+			</button>
+		</a>
 </body>
 </html>
